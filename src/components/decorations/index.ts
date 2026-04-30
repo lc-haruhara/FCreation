6c63924cff07475dce8016509e1c3086493398b0
lc-haruhara <kato@launchcraft.jp>
2026-05-01 04:57:08 +0900

    ReasonSection

--- src/components/decorations/index.ts
+++ src/components/decorations/index.ts
@@ -2,5 +2,6 @@
 export { default as TextLoop } from './texts/TextLoop/TextLoop.astro';
 
 // Backgrounds
-export { default as BackgroundBlocks } from './background/BackgroundBlocks.astro';
-export { default as BackgroundBlocksBottom } from './background/BackgroundBlocksBottom.astro';
+export { default as BackgroundBlocks } from './background/BackgroundBlocks/BackgroundBlocks.astro';
+export { default as BackgroundBlocksBottom } from './background/BackgroundBlocks/BackgroundBlocksBottom.astro';
+export { default as BackgroundDot } from './background/BackgroundDot/BackgroundDot.astro';
